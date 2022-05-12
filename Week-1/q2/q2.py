029217617a0acd7b270d75726a5520f16b714366
pigeon <homoreen@gmail.com>
2022-05-12 14:16:20 +0900

    Edited

--- Week-1/q2/q2.py
+++ Week-1/q2/q2.py
@@ -1,68 +1,76 @@
 import sys
+
+"""
+Usage: $python q2.py input_file output_file
+"""
+
 SIZE = 26   
+SCORES = [1,3,2,2,1,3,3,1,1,4,4,2,2,1,1,3,4,1,1,1,2,3,3,4,3,4]
+
+def read(file):
+    """
+    Returns a list of strings read from each line in given file
+    :type file: .txt
+    :rtype: List[string]
+    """
+    words = file.readlines()
+    words = [line.rstrip() for line in words]
+    return words
 
 def main():
-    input_file= sys.argv[1]
+    input_file = sys.argv[1]
+    output_name = sys.argv[2]
 
-    print("Enter output file name: (no need to include format)")
-    name = input()
-    output_file = open(name + ".txt", 'w+')
+    output_file = open(output_name,'w+')
 
     # Reads words.txt and stores the words into a list
     with open("words.txt") as file:
-        words = file.readlines()
-        words = [line.rstrip() for line in words]
-
+        words = read(file)
     # Reads words.txt and stores the words into a list
     with open(input_file) as test_file:
-        test_words = test_file.readlines()
-        test_words = [line.rstrip() for line in test_words]  
+        test_words = read(test_file)
 
-    scores = [1,3,2,2,1,3,3,1,1,4,4,2,2,1,1,3,4,1,1,1,2,3,3,4,3,4]
-    
     # List to store anagrams with the highest scores
     list = []
 
     # Iterates thru each string from file input
     for test in test_words:
-        freq1 = [0] * SIZE
-        ls = len(test)
+        test_letter_frequency = [0] * SIZE
 
         # Dictionary to assign scores to each anagram
-        test_dict = {}
+        word_scores = {}
         
         # Records frequencies of each char in test
-        for i in range(ls):
-            freq1[ord(test[i]) - ord('a')] += 1
-            max_score = 0
+        for i in range(len(test)):
+            test_letter_frequency[ord(test[i]) - ord('a')] += 1
 
         # Iteratates thru the entire search list
         for i in range(len(words)):
 
             # Declares a new list of frequencies of each word
-            freq2 = [0] * SIZE
-            flag = 0
+            dictionary_word_frequency = [0] * SIZE
+            flag = False 
             score = 0
 
             # Records frequencies of each char in word:
-            for j in range(len(words[i])):
-                freq2[ord(words[i][j]) - ord('a')] += 1
+            for letter in words[i]:
+                dictionary_word_frequency[ord(letter) - ord('a')] += 1
 
 
             # Compares both lists of frequencies
             for k in range(SIZE):
-                if freq2[k] > freq1[k] and freq2[k] >= 1:
-                    flag = 1
+                if dictionary_word_frequency[k] > test_letter_frequency[k] and dictionary_word_frequency[k] >= 1:
+                    flag = True 
                     break
             
             # Calculates score for each anagram found
-            if flag == 0:
+            if flag == False:
                 for k in range(SIZE):
-                    score += freq2[k] * scores[k]
-            test_dict[words[i]] = score
+                    score += dictionary_word_frequency[k] * SCORES[k]
+            word_scores[words[i]] = score
        
         # Finds the anagram with the highest score
-        anagram = max(test_dict, key = test_dict.get)
+        anagram = max(word_scores, key = word_scores.get)
 
         # Appends anagram to list
         list.append(anagram)    
@@ -70,12 +78,11 @@ def main():
     # Transfer elements from list to file output
     for w in list:
         output_file.write(w + "\n")
-
-    # Closes files
-    test_file.close()
-    file.close()
-    output_file.close()
     
     return "File created."
 
-print(main())
+    # Closes file
+    output_file.close()
+
+if __name__ == "__main__":
+    print(main())
